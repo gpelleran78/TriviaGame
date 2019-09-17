3a1f4dcdeaefb038d6a9f74628f8c6c21475804b
gpelleran78 <geremy_pelleran@yahoo.com>
2019-09-16 20:37:12 -0700

    started JS code, link not working

--- assets/javascript/TriviaGame.js
+++ assets/javascript/TriviaGame.js
@@ -1,52 +1,69 @@
-// $(document).ready(function) {
+// $(document).ready(function){
 
-$('#start').on(click, function(){
-    $('#start').remove();
-})
+    $('#start').on(click, function () {
+        $('#start').remove();
+        game.loadQuestion();
+    })
 
-var questions = [{
-    question: "..........",
-    answers: ["......", ".........", "............"],
-    correctAnswer: ".........",
-    image: ".............."
-},
-//repeat above format for each arrey of questions
-{}
-];
+    $(document).on('click', 'answer-button', function (e) {
+        game.clicked(e);
+    })
 
-
-var game = {
-    question:questions,
-    currentQuestion:0,
-    counter:30,
-    courrect:0,
-    incourrect:0,
-
-    countdown: function(){
-
+    var questions = [{
+        question: "..........",
+        answers: ["......", ".........", "............"],
+        correctAnswer: ".........",
+        image: ".............."
     },
-    loadQuestion: function(){
+    //repeat above format for each arrey of questions
+    {}
+    ];
 
-    },
-    nextQuestion: function(){
 
-    },
-    timeUp: function(){
+    var game = {
+        question: questions,
+        currentQuestion: 0,
+        counter: 30,
+        courrect: 0,
+        incourrect: 0,
 
-    },
-    result: function(){
+        countdown: function () {
+            game.counter--;
+            $('#counter').html(game.counter);
+            if (game.counter <= 0) {
+                console.log("Time Up!!!");
+                game.timeUp();
+            }
 
-    },
-    clicked: function(){
+        },
+        loadQuestion: function () {
+            timer = setInterval(game.countdown, 1000);
+            $('#subwrapper').html('<h2>' + question[game.currentQuestion].question + '</h2>');
+            for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
+                $('#subwrapper').append('button class= "answer-button" id=" button-' + i + ' "data-name" ' +
+                    question[game.currentQuestion].answer[i] + '">' + question[game.currentQuestion].answer[i] + ' </button> ');
 
-    },
-    answeredCorrectly: function(){
+            }
+        },
+        nextQuestion: function () {
 
-    },
-    answeredIncorrectly: function(){
+        },
+        timeUp: function () {
 
-    },
-    reset: function(){
+        },
+        result: function () {
 
+        },
+        clicked: function () {
+
+        },
+        answeredCorrectly: function () {
+
+        },
+        answeredIncorrectly: function () {
+
+        },
+        reset: function () {
+
+        }
     }
-}
