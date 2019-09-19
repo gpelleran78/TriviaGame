0bf53d1586bda755b2751e07c7f03fc203a124c1
gpelleran78 <geremy_pelleran@yahoo.com>
2019-09-18 20:43:27 -0700

    working prototype

--- assets/javascript/TriviaGame.js
+++ assets/javascript/TriviaGame.js
@@ -1,26 +1,32 @@
-$(document).ready(function (){
-     $('#start').on('click', function () {
-         $('#start').remove();
-    //     game.loadQuestion();
-     });
+$(document).ready(function () {
+    $('#start').on('click', function () {
+        $('#start').remove();
+        game.loadQuestion();
+    });
 
-    $(document).on('click', 'answer-button', function (e) {
+    $(document).on('click', '.answer-button', function (e) {
         game.clicked(e);
     });
 
     var questions = [{
-        question: "..........",
-        answers: ["......", ".........", "............"],
-        correctAnswer: ".........",
+        question: "What city were you born in",
+        answers: ["Alpena", "Flint", "Marquette"],
+        correctAnswer: "Alpena",
         image: ".............."
     },
     //repeat above format for each arrey of questions
+    {
+        question: "Where do you live now?",
+        answers: ["Alpena", "Flint", "Marquette", "Phoenix"],
+        correctAnswer: "Phoenix",
+        image: ".............."
+    },
     {}
     ];
 
-console.log(questions)
+    // console.log(questions)
     var game = {
-        question: questions,
+        questions: questions,
         currentQuestion: 0,
         counter: 30,
         courrect: 0,
@@ -37,15 +43,18 @@ console.log(questions)
         },
         loadQuestion: function () {
             timer = setInterval(game.countdown, 1000);
-            $('#subwrapper').html('<h2>' + question[game.currentQuestion].question + '</h2>');
+            $('#subwrapper').html('<h2>' + questions[game.currentQuestion].question + '</h2>');
             for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
-                $('#subwrapper').append('button class= "answer-button" id=" button-' + i + ' "data-name" ' +
-                    question[game.currentQuestion].answer[i] + '">' + question[game.currentQuestion].answer[i] + ' </button> ');
+                $('#subwrapper').append('<button class= "answer-button" id="button- ' + i + '" data-name= "' +
+                    questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>');
 
             }
         },
         nextQuestion: function () {
-
+            game.counter - 30;
+            $("#counter").html(game.counter);
+            game.currentQuestion++;
+            game.loadQuestion();
         },
         timeUp: function () {
 
@@ -53,14 +62,39 @@ console.log(questions)
         result: function () {
 
         },
-        clicked: function () {
+        clicked: function (e) {
+            clearInterval(timer);
+            if ($(e.target).data("name") == questions[game.currentQuestion].correctAnswer) {
+                game.answeredCorrectly();
+            }
+            else {
+                game.answeredIncorrectly();
+            }
 
         },
         answeredCorrectly: function () {
-
+            console.log("you are correct")
+            clearInterval(timer);
+            game.correct++;
+            $('#subwrapper').html('<h2>YOU GOT IT RIGHT!</H2>');
+            if (game.currentQuestion == questions.length - 1) {
+                setTimeout(game.result, 3 * 1000);
+            }
+            else {
+                setTimeout(game.nextQuestion, 3 * 1000);
+            }
         },
         answeredIncorrectly: function () {
-
+            console.log("Wrong Answer")
+            clearInterval(timer);
+            game.incorrect++;
+            $('#subwrapper').html('<h2>YOU GOT IT WRONG!</H2>');
+            if (game.currentQuestion == questions.length - 1) {
+                setTimeout(game.result, 3 * 1000);
+            }
+            else {
+                setTimeout(game.nextQuestion, 3 * 1000);
+            }
         },
         reset: function () {
 
